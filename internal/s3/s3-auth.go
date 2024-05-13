7342b8e15dd4f97802e5c51f9e6107bb9c9949e2
Pradeep Peiris <wpnpeiris@gmail.com>
2024-05-13 21:13:04 +0200

    Enable auth interceptor

---
+++ internal/s3/s3-auth.go
@@ -0,0 +1,5 @@
+/*
+ * The following code is mostly copied from seaweedfs (https://github.com/seaweedfs/seaweedfs/blob/master/weed/s3api/auth_credentials.go) implementation.
+ */
+
+package s3
--- internal/s3/s3-gateway.go
+++ internal/s3/s3-gateway.go
@@ -4,6 +4,8 @@ import (
 	"log"
 
 	"github.com/gorilla/mux"
+	s3api "github.com/seaweedfs/seaweedfs/weed/s3api"
+	s3cont "github.com/seaweedfs/seaweedfs/weed/s3api/s3_constants"
 	"github.com/wpnpeiris/nats-gateway/internal/client"
 )
 
@@ -29,9 +31,11 @@ func (s3 S3Gateway) RegisterS3Routes(router *mux.Router) {
 
 	s3Router := router.PathPrefix("/").Subrouter()
 
+	iam := &s3api.IdentityAccessManagement{}
+
 	s3Router.Methods("OPTIONS").HandlerFunc(s3.SetOptionHeaders)
 
-	s3Router.Methods("HEAD").Path("/{bucket}/{key}").HandlerFunc(s3.HeadObject)
+	s3Router.Methods("HEAD").Path("/{bucket}/{key}").HandlerFunc(iam.Auth(s3.HeadObject, s3cont.ACTION_LIST))
 
 	s3Router.Methods("GET").Path("/").HandlerFunc(s3.ListBuckets)
 	s3Router.Methods("GET").Path("/{bucket}").HandlerFunc(s3.ListObjects)
